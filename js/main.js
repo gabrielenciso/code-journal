67e0d0a5a842774a2e607f2c7fdb0b2e098db4db
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-04 23:41:04 +0000

    listen for submit and add input values to object

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 /* global data */
 /* exported data */
 
-var $photoURLInput = document.querySelector('#photo-url');
+var $photoURLInput = document.querySelector('#photoUrl');
 var $photoPreviewSRC = document.querySelector('.photo-preview img');
+var $form = document.querySelector('form');
 console.log($photoURLInput);
 console.log($photoPreviewSRC);
+console.log($form);
 
 function handlePhotoUpdate(event) {
   $photoPreviewSRC.setAttribute('src', event.target.value);
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  var formResult = {};
+  formResult[$form.elements.title.name] = $form.elements.title.value;
+  formResult[$form.elements.photoUrl.name] = $form.elements.photoUrl.value;
+  formResult[$form.elements.notes.name] = $form.elements.notes.value;
+
+  console.log('form results: ', formResult);
+  $photoPreviewSRC.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
 $photoURLInput.addEventListener('input', handlePhotoUpdate);
+$form.addEventListener('submit', handleSubmit);
