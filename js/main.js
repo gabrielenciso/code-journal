f58a3bb832d247f18daa8b83b846e975003852df
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-05 18:31:15 +0000

    made function that takes single entry object and returns DOM tree

--- js/main.js
+++ js/main.js
@@ -25,5 +25,50 @@ function handleSubmit(event) {
   $form.reset();
 }
 
+function makeEntry(entries) {
+  // <li class="row">
+  //   <div class="photo-preview column-half">
+  //     <img src="images/placeholder-image-square.jpg">
+  //   </div>
+  //   <div class="column-half padding-left-entries">
+  //     <h2 class="font-form">Ada Lovelace</h2>
+  //     <p>
+  //       Augusta Ada King, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles
+  //       Babbage's proposed mechanical general-purpose computer, the Analytical Engine.She was the first to recognize that the
+  //       machine had applications beyond pure calculation, and to have published the first algorithm intended to be carried out
+  //       by such a machine.
+  //     </p>
+  //   </div>
+  // </li>
+  var $entry = document.createElement('li');
+  $entry.setAttribute('class', 'row');
+
+  var $photoView = document.createElement('div');
+  $photoView.setAttribute('class', 'photo-preview column-half');
+
+  var $image = document.createElement('img');
+  $image.setAttribute('src', entries.photoUrl);
+
+  $photoView.appendChild($image);
+
+  var $divText = document.createElement('div');
+  $divText.setAttribute('class', 'column-half padding-left-entries');
+
+  $entry.appendChild($photoView);
+  $entry.appendChild($divText);
+
+  var $title = document.createElement('h2');
+  $title.setAttribute('class', 'font-form ');
+  $title.textContent = entries.title;
+
+  var $text = document.createElement('p');
+  $text.textContent = entries.notes;
+
+  $divText.appendChild($title);
+  $divText.appendChild($text);
+
+  return $entry;
+}
+
 $photoURLInput.addEventListener('input', handlePhotoUpdate);
 $form.addEventListener('submit', handleSubmit);
